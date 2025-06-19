1ce3f8fc4560f2da8e7b4b93336ea322cbfe7afd
Eleas Kanchon <eleas@softzino.com>
2025-06-19 10:41:09 +0600

    quick fix

--- Basic CRUD Example/js/fetch-cpt.js
+++ Basic CRUD Example/js/fetch-cpt.js
@@ -8,11 +8,12 @@ document.addEventListener('DOMContentLoaded', function () {
     .then(books => {
       const container = document.getElementById('posts-container');
       books.forEach(book => {
+        console.log(book);
         const card = document.createElement('div');
         card.className = 'card';
         card.innerHTML = `
           <h2>${book.title?.rendered}</h2>
-          <div>${book.excerpt?.rendered}</div>
+          <div>${book.content.rendered}</div>
           <a href="post-details.html?id=${book.id}" target="_blank">Read More</a>
           <br><button onclick="deletePost(${book.id})">Delete</button>
         `;
